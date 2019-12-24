92175c1fd5246cc03a785015dd3e26e2c69c8586
OD185 <peeradon_p@cmu.ac.th>
2019-12-24 15:59:13 +0700

    update

---
+++ src/pages/inbox.js
@@ -0,0 +1,7 @@
+import Inboxscreen from '../screen/inbox/Inbox'
+
+const Inbox = () => (
+    <Inboxscreen />
+);
+
+export default Inbox;
---
+++ src/screen/inbox/Inbox.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Inbox extends React.Component {
+    render() {
+        let asd = "Teeradon"
+        return <h1>Hello, {asd}!</h1>
+    }
+}
+
+export default Inbox
